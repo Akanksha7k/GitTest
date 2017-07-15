68a29d673d9bf1056c3405caa8783ece4459e912
Akanksha7k <akanksha.khare7@gmail.com>
2017-07-15 20:57:34 +0530

    Test

--- TestRepoGit/src/Main.java
+++ TestRepoGit/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("This is a test class");
+		System.out.println("This is a test github class");
 
 	}
 
