b05fea01c7011cd91f2e1959bd6fa1657d80176f
spdeo1 <satyaprakashdeo@gmail.com>
2017-07-16 13:24:14 +0530

    Test

--- TestRepoGit/src/Main.java
+++ TestRepoGit/src/Main.java
@@ -2,10 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		System.out.println("This is a test github class");
-
+		// TODO Auto-generated method stub1
+		for(int i=0;i<100;i++){
+		System.out.println(Secret.feeling);
+		}
 	}
 
 }
